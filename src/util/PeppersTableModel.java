6094717e13267f1e9ffb575922054336312008c5
Aluno <Aluno@labHP-PC>
2015-10-20 22:18:55 -0200

    Campo tabela não editavel

--- src/util/PeppersTableModel.java
+++ src/util/PeppersTableModel.java
@@ -5,8 +5,10 @@ import javax.swing.table.DefaultTableModel;
 public class PeppersTableModel extends DefaultTableModel{
 
 	
-	/*@Override
+	
 	public boolean isCellEditable(int row, int column) {
 		return false;
-	};*/
+
 }
+	
+}	
